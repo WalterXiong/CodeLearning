3b36455b1b181916d04839c715b932684c4952ef
walterXiong <522500824@qq.com>
2022-05-17 15:27:30 +0800

    delete 'dist' in config.ts

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -2,16 +2,9 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme";
 
 export default defineUserConfig({
-  port: 8080,
   lang: "zh-CN",
-  title: "主题演示",
-  description: "vuepress-theme-hope 的演示",
-
-  //指定 vuepress build 的输出目录
-  dest: "docs/.vuepress/dist",
-
-  // 是否开启默认预加载 js
-  shouldPrefetch: (file, type) => false,
+  title: "CodeLearning",
+  description: "This is a learning project",
 
   base: "/CodeLearing/",
 
